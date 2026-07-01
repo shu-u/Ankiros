b23bb25e8aa4718e00e3187ef1c04c08550a8ca2
Kazuki Arima <p984172@keyence.co.jp>
2026-07-01 13:13:54 +0900

    (android) disable zoom

--- src-tauri/gen/android/app/src/main/java/com/p984172/ankiros/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/p984172/ankiros/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.p984172.ankiros
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+  }
+
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+    webView.settings.apply {
+      setSupportZoom(false)
+      builtInZoomControls = false
+      displayZoomControls = false
+    }
   }
 }
